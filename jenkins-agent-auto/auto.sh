fabc06fa75cf8d18484267c5329e7d04d3ab3b39
Cédric Ollivier <cedric.ollivier@orange.com>
2021-01-08 21:45:50 +0100

    Allow modifying agent name
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

--- jenkins-agent-auto/auto.sh
+++ jenkins-agent-auto/auto.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 curl -k -fsSLo /usr/share/jenkins/jenkins-cli.jar $JENKINS_URL/jnlpJars/jenkins-cli.jar
-java -jar /usr/share/jenkins/jenkins-cli.jar -auth admin:admin -s $JENKINS_URL create-node xtesting << EOF
+java -jar /usr/share/jenkins/jenkins-cli.jar -auth admin:admin -s $JENKINS_URL create-node ${JENKINS_SLAVE_NAME:-xtesting} << EOF
 <slave><launcher class='hudson.slaves.JNLPLauncher' /><mode>NORMAL</mode><numExecutors>2</numExecutors><remoteFS>/var/jenkins_home</remoteFS></slave>
 EOF
-java -jar /usr/share/jenkins/agent.jar -jnlpUrl $JENKINS_URL/computer/xtesting/slave-agent.jnlp -jnlpCredentials admin:admin
+java -jar /usr/share/jenkins/agent.jar -jnlpUrl $JENKINS_URL/computer/${JENKINS_SLAVE_NAME:-xtesting}/slave-agent.jnlp -jnlpCredentials admin:admin
